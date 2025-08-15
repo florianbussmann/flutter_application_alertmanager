73b38c04cba28c8293584b7baafd3b1f1d84ee5d
florianbussmann <florianbussmann@users.noreply.github.com>
2025-08-15 21:06:04 +0000

    deploy: 077e856ae0625519cf57f98d344ed612afbca538

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "32a8e3bf7d9912fa605db7afaf921a31",
+const RESOURCES = {"flutter_bootstrap.js": "386e5a339fb99fdde0f91ae3edfaa065",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
