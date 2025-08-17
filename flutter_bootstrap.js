c8999fefef629d45d96bd95c9679cf520b2ef157
florianbussmann <florianbussmann@users.noreply.github.com>
2025-08-17 17:37:31 +0000

    deploy: 93437a37d16b98f8b9f569ccfd2ae3a3ba834b66

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3501335611"
+    serviceWorkerVersion: "1976372465"
   }
 });
